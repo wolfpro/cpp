e5e2e953e67ef70dd915391bfdf766a2753b5fac
wolfpro <wolfpro357601@gmail.com>
2015-03-24 18:04:59 +0300

    wolfpro

--- UI_CUP_2014/Local_Runner/local-runner-console.bat
+++
@@ -1,1 +0,0 @@
-java -cp ".;*;%~dp0/*" -jar "local-runner.jar" local-runner-console.properties
--- pow.c
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main()
-{
-    for (int i = 0; i < 8; i++) {
-        std::cout << "y*";
-    }
-    return 0;
-}
